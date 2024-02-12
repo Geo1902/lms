13f5d83a40bf740a9d59976ff611e4cbb8d7217c
Artem <georgich1902@gmail.com>
2024-02-12 19:42:21 +0300

    get and set doneee

--- src/Course.java
+++ src/Course.java
@@ -10,4 +10,23 @@ public class Course {
         this.id = lastId++;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
